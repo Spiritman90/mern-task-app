c4e03e9f15cbda0be0b3206e1c71bbbfbf31f6f5
spiritman90 <phemmyadey@gmail.com>
2022-12-29 10:28:47 +0100

    Connected DB and created task model

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
