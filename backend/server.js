f084c04dd5303ce01802fe975216e60039483d4c
spiritman90 <phemmyadey@gmail.com>
2023-01-03 08:55:30 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000/"],
+  })
+);
 app.use("/api/tasks", taskRoutes);
 
 //Routes
