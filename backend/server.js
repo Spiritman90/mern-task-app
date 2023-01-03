82a11a41b1cb1af797ab4a4416840754fe9f45a2
spiritman90 <phemmyadey@gmail.com>
2023-01-03 08:43:52 +0100

    Completed Project

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const connectDB = require("./config/connectDB");
 const Task = require("./models/taskModel");
 const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/api/tasks", taskRoutes);
 
 //Routes
